fa73c54e079f2bbe79e2eb63fc9a79cc705f42da
unknown <vasudevayalamanchi@gmail.com>
2025-05-30 05:57:50 +0530

    commit

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 import pytest
+import logging
+import os
+import time
 from pages.login import LoginPage
 from pages.homepage import HomePage
 
@@ -13,6 +16,7 @@ def driver():
     yield driver
     driver.quit()
 
+
 @pytest.fixture()
 def login(request, driver):
     username, password = request.param
@@ -23,8 +27,9 @@ def login(request, driver):
     login_page.enter_username(username)
     login_page.enter_password(password)
     login_page.click_signin_button()
-
+    
     yield driver
+
 
 def pytest_addoption(parser):
     parser.addini(
@@ -39,6 +44,7 @@ def pytest_addoption(parser):
         type="linelist"
     )
 
+
 def pytest_generate_tests(metafunc):
     if metafunc.function.__name__ == "test_TC_signup_001":
         data = metafunc.config.getini("TC_signup_001")
@@ -49,3 +55,57 @@ def pytest_generate_tests(metafunc):
         data = metafunc.config.getini("TC_signin_001")
         params = [tuple(line.split(",")) for line in data]
         metafunc.parametrize(("username", "password"), params)
+
+
+@pytest.fixture(scope="session")
+def logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler(f"./data/QA_Automation.log")
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+
+def capture_screen_shot(name):
+    driver.get_screenshot_as_file(name)
+
+
+# This hook is capture the screenshot upon failure and add the screenshot to the respective TC in report
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    try:
+        # Instantiate the pytest plugin manager for html
+        pytest_html = item.config.pluginmanager.get_plugin("html")
+        outcome = yield
+        # get the current test case result
+        report = outcome.get_result()
+        extra = getattr(report, "extra", [])
+        # At Setup phase or when framework calls it to update report we need to capture screenshot only on failure
+        if report.when == "call" or report.when == "setup":
+            xfail = hasattr(report, "wasxfail")
+            if (report.skipped and xfail) or (report.failed and not xfail):
+                # get all screenshots from current test case
+                screen_shots = item.funcargs["screen_shots_list"]
+                for i, file_name in enumerate(screen_shots):
+                    # If file exist, then add the extra hyperlink that can show the screen shot captured
+                    if file_name:
+                        html = f'<a href="{file_name}">Failure Screenshot {i+1}</a>'
+                        extra.append(pytest_html.extras.html(html))
+                # Upon failure, capture the last screen where it is terminated
+                file_name_with_out_path = report.nodeid.split("/")[-1]
+                file = (
+                    file_name_with_out_path.replace("::", "_")
+                    + str(int(time.time()))
+                    + ".png"
+                )
+                # Define the custom path where to store the screen shots
+                file_name = os.path.join(os.getcwd(), "data", "test_result", file)
+                capture_screen_shot(file_name)
+                if file_name:
+                    html = f'<a href="{file}">Test case Termination Screenshot</a>'
+                    extra.append(pytest_html.extras.html(html))
+            report.extras = extra
+    except Exception as e:
+        print("Exception occured in Pytest makereport Hook")
--- testSuites/Signup/test_signup.py
+++ testSuites/Signup/test_signup.py
@@ -2,8 +2,9 @@ from pages.signup import SignupForm
 from pages.homepage import HomePage
 
 
-def test_TC_signup_001(driver, email, username, password, message):
+def test_TC_signup_001(driver, logger, email, username, password, message):
     try:
+        logger.info("********* test_TC_signup_001 ********")
         homepage = HomePage(driver)
         homepage.click_signup_button()
         signup_page = SignupForm(driver)
@@ -14,4 +15,5 @@ def test_TC_signup_001(driver, email, username, password, message):
         assert message in signup_page.read_blank_email_msg() or signup_page.read_blank_pwd_msg() or signup_page.read_blank_usrname_msg()
     
     except Exception as e:
-        print("[Exception Occured] " + str(e))
+        logger.info("[Exception Occured] " + str(e))
+        assert False, "Exception occured, hence test case is failed"
